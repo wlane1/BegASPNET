f22b4829b77003c61cd0a49858e1dffb556c8e45
wlane1 <wlane1@mail.usf.edu>
2016-06-23 15:22:17 -0400

    Add files via upload

---
+++ BegASPNET/Site/Demos/CodeBehind.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Demos_CodeBehind : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        IntroLabel.Text = "Label Test. The date & time is now " + DateTime.Now.ToString();
+    }
+}
